6ebdedf4c0e19e4dfc1852c51025d9a46e3922c5
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2025-05-05 23:36:36 -0400

    started developing landing page

---
+++ frontend/learning-system-ui/src/App.js
@@ -0,0 +1,13 @@
+
+import logo from './logo.svg';
+import './App.css';
+
+import { LandingPage } from './Components/LandingPage/LandingPage.tsx';
+
+function App() {
+  return (
+      <LandingPage/>
+  );
+}
+
+export default App;
---
+++ frontend/learning-system-ui/src/Components/LandingPage/LandingPage.tsx
@@ -0,0 +1,37 @@
+
+import { Container, Box, minHeight, width } from "@mui/system";
+import React from "react";
+
+
+
+export const LandingPage = ()=>{
+
+
+    return (
+        
+
+        <div>
+            <Box
+                component="img"
+                src="/Harvard.PNG" 
+                alt="Image of Harvard"
+                sx={{
+                    minHeight:'100vh',
+                    textAlign:'left',
+                    width:'70%'
+                    
+                }}
+            />
+
+        </div>
+
+        
+    )
+
+}
+
+
+
+
+
+
